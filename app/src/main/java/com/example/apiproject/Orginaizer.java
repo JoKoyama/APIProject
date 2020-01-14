31220745afc2ed973674d3908b4185c6a7dfc405
Jo <jkoyama232@gmail.com>
2020-01-14 13:50:43 -0800

    Initial commit made layouts for list and detail pages

---
+++ app/src/main/java/com/example/apiproject/Orginaizer.java
@@ -0,0 +1,40 @@
+package com.example.apiproject;
+
+public class Orginaizer {
+    private String title;
+    private String genre;
+    private String episodes;
+    private String airingSeason;
+    private String producer;
+    private String animationStudio;
+    private String synopsis;
+    private String malScore;
+    private String status;
+    private String statusString;
+
+    public Orginaizer(){
+
+    }
+
+
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+    public String getGenre() { return genre; }
+    public void setGenre(String genre) { this.genre = genre; }
+    public String getEpisodes() { return episodes; }
+    public void setEpisodes(String episodes) { this.episodes = episodes; }
+    public String getAiringSeason() { return airingSeason; }
+    public void setAiringSeason(String airingSeason) { this.airingSeason = airingSeason; }
+    public String getProducer() { return producer; }
+    public void setProducer(String producer) { this.producer = producer; }
+    public String getAnimationStudio() { return animationStudio; }
+    public void setAnimationStudio(String animationStudio) { this.animationStudio = animationStudio; }
+    public String getSynopsis() { return synopsis; }
+    public void setSynopsis(String synopsis) { this.synopsis = synopsis; }
+    public String getMalScore() { return malScore; }
+    public void setMalScore(String malScore) { this.malScore = malScore; }
+    public String getStatus() { return status; }
+    public void setStatus(String status) { this.status = status; }
+    public String getStatusString() { return statusString; }
+    public void setStatusString(String statusString) { this.statusString = statusString; }
+}
